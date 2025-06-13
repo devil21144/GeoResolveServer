19c59fb28244300c84af1df270f4996dd5847c38
Dakshina <mihirvelaga21144@gmail.com>
2025-06-13 13:07:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 const db = require("./database");
 const app = express();
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const transport = nodemailer.createTransport({
   host: "gmail",
   auth: {
@@ -37,8 +37,22 @@ app.post("/register/citizen", async (req, res) => {
       ]
     );
     transport.sendMail({
-      
-    })
+      from: "'GeoResolve' <mihirvelaga21144@gmail.com>",
+      to: req.body.email,
+      subject: "OTP Verification",
+      text: `Dear ${req.body.username} ,
+
+Your One-Time Password (OTP) for verification is:
+
+🔐 OTP: ${number}
+
+This code is valid for the next 10 minutes. Please do not share this OTP with anyone.
+
+If you did not request this code, please ignore this message.
+
+Thank you,
+GeoResolve Team`,
+    });
     res.status(200).send({
       message: "successful",
       status: "ok",
@@ -53,6 +67,61 @@ app.post("/register/citizen", async (req, res) => {
     });
   }
 });
+app.post("/register/citizen", async (req, res) => {
+  const number = Math.floor(Math.random() * 999999) + 100000;
+  const password = req.body.password;
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hashed = await bcrypt.hash(password, salt);
+  console.log(req.body);
+  try {
+    const results = await db.query(
+      "insert into assigntable values(lower($1),$2,lower($3),$4,$5,$6,$7,lower($8),lower($9),lower($10))",
+      [
+        req.body.username,
+        hashed,
+        req.body.email,
+        number,
+        req.body.phoneno,
+        "authority",
+        req.body.age,
+        req.body.district,
+        req.body.village,
+        req.body.role,
+      ]
+    );
+    transport.sendMail({
+      from: "'GeoResolve' <mihirvelaga21144@gmail.com>",
+      to: req.body.email,
+      subject: "OTP Verification",
+      text: `Dear ${req.body.username} ,
+
+Your One-Time Password (OTP) for verification is:
+
+🔐 OTP: ${number}
+
+This code is valid for the next 10 minutes. Please do not share this OTP with anyone.
+
+If you did not request this code, please ignore this message.
+
+Thank you,
+GeoResolve Team`,
+    });
+    res.status(200).send({
+      message: "successful",
+      status: "ok",
+    });
+  } catch (err) {
+    console.log(err);
+    console.log(err.statusCode);
+    const status = err.statusCode || 500;
+    res.status(status).send({
+      message: err.message,
+      status: "error",
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server up and listening on ${port}`);
 });
